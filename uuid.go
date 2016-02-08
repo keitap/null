3f15aa56d3fd852fc28a104ba783905b5779d982
Keita Kitamura <keita@sumally.com>
2016-02-08 19:22:53 +0900

    implements null.UUID.

---
+++ uuid.go
@@ -0,0 +1,140 @@
+package null
+
+import (
+	"database/sql/driver"
+	"encoding/json"
+	"fmt"
+	"reflect"
+
+	"github.com/satori/go.uuid"
+)
+
+// UUID is a nullable uuid.UUID. It supports SQL and JSON serialization.
+// It will marshal to null if null.
+type UUID struct {
+	UUID  uuid.UUID
+	Valid bool
+}
+
+// Scan implements the Scanner interface.
+func (u *UUID) Scan(value interface{}) error {
+	var err error
+	switch x := value.(type) {
+	case uuid.UUID:
+		u.UUID = x
+	case nil:
+		u.Valid = false
+		return nil
+	default:
+		err = fmt.Errorf("null: cannot scan type %T into null.UUID: %v", value, value)
+	}
+	u.Valid = err == nil
+	return err
+}
+
+// Value implements the driver Valuer interface.
+func (u UUID) Value() (driver.Value, error) {
+	if !u.Valid {
+		return nil, nil
+	}
+	return u.UUID, nil
+}
+
+// NewUUID creates a new UUID.
+func NewUUID(u uuid.UUID, valid bool) UUID {
+	if u == uuid.Nil {
+		valid = false
+	}
+
+	return UUID{
+		UUID:  u,
+		Valid: valid,
+	}
+}
+
+// UUIDFrom creates a new UUID that will always be valid.
+func UUIDFrom(u uuid.UUID) UUID {
+	return NewUUID(u, true)
+}
+
+// UUIDFromPtr creates a new UUID that will be null if t is nil.
+func UUIDFromPtr(u *uuid.UUID) UUID {
+	if u == nil {
+		return NewUUID(uuid.UUID{}, false)
+	}
+	return NewUUID(*u, true)
+}
+
+// MarshalJSON implements json.Marshaler.
+// It will encode null if this uuid is null.
+func (u UUID) MarshalJSON() ([]byte, error) {
+	if !u.Valid {
+		return []byte("null"), nil
+	}
+	return []byte(`"` + u.UUID.String() + `"`), nil
+}
+
+// UnmarshalJSON implements json.Unmarshaler.
+// It supports string, object (e.g. pq.NullUUID and friends)
+// and null input.
+func (u *UUID) UnmarshalJSON(data []byte) error {
+	var err error
+	var v interface{}
+	if err = json.Unmarshal(data, &v); err != nil {
+		return err
+	}
+	switch x := v.(type) {
+	case string:
+		err = u.UUID.UnmarshalText([]byte(x))
+	case map[string]interface{}:
+		ui, uiOK := x["UUID"].(string)
+		valid, validOK := x["Valid"].(bool)
+		if !uiOK || !validOK {
+			return fmt.Errorf(`json: unmarshalling object into Go value of type null.UUID requires key "UUID" to be of type string and key "Valid" to be of type bool; found %T and %T, respectively`, x["UUID"], x["Valid"])
+		}
+		err = u.UUID.UnmarshalText([]byte(ui))
+		u.Valid = valid
+		return err
+	case nil:
+		u.Valid = false
+		return nil
+	default:
+		err = fmt.Errorf("json: cannot unmarshal %v into Go value of type null.UUID", reflect.TypeOf(v).Name())
+	}
+	u.Valid = err == nil
+	return err
+}
+
+func (u UUID) MarshalText() ([]byte, error) {
+	if !u.Valid {
+		return []byte("null"), nil
+	}
+	return u.UUID.MarshalText()
+}
+
+func (u *UUID) UnmarshalText(text []byte) error {
+	str := string(text)
+	if str == "" || str == "null" {
+		u.Valid = false
+		return nil
+	}
+	if err := u.UUID.UnmarshalText(text); err != nil {
+		return err
+	}
+	u.Valid = true
+	return nil
+}
+
+// SetValid changes this UUID's value and sets it to be non-null.
+func (u *UUID) SetValid(v uuid.UUID) {
+	u.UUID = v
+	u.Valid = true
+}
+
+// Ptr returns a pointer to this UUID's value, or a nil pointer if this UUID is null.
+func (u UUID) Ptr() *uuid.UUID {
+	if !u.Valid {
+		return nil
+	}
+	return &u.UUID
+}
---
+++ uuid_test.go
@@ -0,0 +1,185 @@
+package null
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/satori/go.uuid"
+)
+
+var (
+	uuidString     = "e0e8d676-ce47-11e5-ab30-625662870761"
+	uuidJSON       = []byte(`"` + uuidString + `"`)
+	nullUUIDJSON   = []byte(`null`)
+	uuidValue, _   = uuid.FromString(uuidString)
+	uuidObject     = []byte(`{"UUID":"e0e8d676-ce47-11e5-ab30-625662870761","Valid":true}`)
+	uuidNullObject = []byte(`{"UUID":"e0e8d676-ce47-11e5-ab30-625662870761","Valid":false}`)
+	uuidBadObject  = []byte(`{"hello": "world"}`)
+)
+
+func TestUnmarshalUUIDJSON(t *testing.T) {
+	var ui UUID
+	err := json.Unmarshal(uuidJSON, &ui)
+	maybePanic(err)
+	assertUUID(t, ui, "UnmarshalJSON() json")
+
+	var null UUID
+	err = json.Unmarshal(nullUUIDJSON, &null)
+	maybePanic(err)
+	assertNullUUID(t, null, "null uuid json")
+
+	var fromObject UUID
+	err = json.Unmarshal(uuidObject, &fromObject)
+	maybePanic(err)
+	assertUUID(t, fromObject, "uuid from object json")
+
+	var nullFromObj UUID
+	err = json.Unmarshal(uuidNullObject, &nullFromObj)
+	maybePanic(err)
+	assertNullUUID(t, nullFromObj, "null from object json")
+
+	var invalid UUID
+	err = invalid.UnmarshalJSON(invalidJSON)
+	if _, ok := err.(*json.SyntaxError); !ok {
+		t.Errorf("expected json.SyntaxError, not %T", err)
+	}
+	assertNullUUID(t, invalid, "invalid from object json")
+
+	var bad UUID
+	err = json.Unmarshal(uuidBadObject, &bad)
+	if err == nil {
+		t.Errorf("expected error: bad object")
+	}
+	assertNullUUID(t, bad, "bad from object json")
+
+	var wrongType UUID
+	err = json.Unmarshal(intJSON, &wrongType)
+	if err == nil {
+		t.Errorf("expected error: wrong type JSON")
+	}
+	assertNullUUID(t, wrongType, "wrong type object json")
+}
+
+func TestUnmarshalUUIDText(t *testing.T) {
+	u := UUIDFrom(uuidValue)
+	txt, err := u.MarshalText()
+	maybePanic(err)
+	assertJSONEquals(t, txt, uuidString, "marshal text")
+
+	var unmarshal UUID
+	err = unmarshal.UnmarshalText(txt)
+	maybePanic(err)
+	assertUUID(t, unmarshal, "unmarshal text")
+
+	u = UUIDFrom(uuid.Nil)
+	txt, err = u.MarshalText()
+	maybePanic(err)
+	assertJSONEquals(t, txt, string(nullUUIDJSON), "marshal text")
+
+	err = unmarshal.UnmarshalText(txt)
+	maybePanic(err)
+	assertNullUUID(t, unmarshal, "unmarshal text")
+
+	var null UUID
+	err = null.UnmarshalText(nullJSON)
+	maybePanic(err)
+	assertNullUUID(t, null, "unmarshal null text")
+	txt, err = null.MarshalText()
+	maybePanic(err)
+	assertJSONEquals(t, txt, string(nullJSON), "marshal null text")
+
+	var invalid UUID
+	err = invalid.UnmarshalText([]byte("hello world"))
+	if err == nil {
+		t.Error("expected error")
+	}
+	assertNullUUID(t, invalid, "bad string")
+}
+
+func TestMarshalUUID(t *testing.T) {
+	ti := UUIDFrom(uuidValue)
+	data, err := json.Marshal(ti)
+	maybePanic(err)
+	assertJSONEquals(t, data, string(uuidJSON), "non-empty json marshal")
+
+	ti.Valid = false
+	data, err = json.Marshal(ti)
+	maybePanic(err)
+	assertJSONEquals(t, data, string(nullJSON), "null json marshal")
+}
+
+func TestUUIDFrom(t *testing.T) {
+	ti := UUIDFrom(uuidValue)
+	assertUUID(t, ti, "UUIDFrom() uuid.UUID")
+}
+
+func TestUUIDFromPtr(t *testing.T) {
+	ti := UUIDFromPtr(&uuidValue)
+	assertUUID(t, ti, "UUIDFromPtr() uuid")
+
+	null := UUIDFromPtr(nil)
+	assertNullUUID(t, null, "UUIDFromPtr(nil)")
+}
+
+func TestUUIDSetValid(t *testing.T) {
+	var ti uuid.UUID
+	change := NewUUID(ti, false)
+	assertNullUUID(t, change, "SetValid()")
+	change.SetValid(uuidValue)
+	assertUUID(t, change, "SetValid()")
+}
+
+func TestUUIDPointer(t *testing.T) {
+	ti := UUIDFrom(uuidValue)
+	ptr := ti.Ptr()
+	if *ptr != uuidValue {
+		t.Errorf("bad %s uuid: %#v ≠ %v\n", "pointer", ptr, uuidValue)
+	}
+
+	var nt uuid.UUID
+	null := NewUUID(nt, false)
+	ptr = null.Ptr()
+	if ptr != nil {
+		t.Errorf("bad %s uuid: %#v ≠ %s\n", "nil pointer", ptr, "nil")
+	}
+}
+
+func TestUUIDScanValue(t *testing.T) {
+	var ti UUID
+	err := ti.Scan(uuidValue)
+	maybePanic(err)
+	assertUUID(t, ti, "scanned uuid")
+	if v, err := ti.Value(); v != uuidValue || err != nil {
+		t.Error("bad value or err:", v, err)
+	}
+
+	var null UUID
+	err = null.Scan(nil)
+	maybePanic(err)
+	assertNullUUID(t, null, "scanned null")
+	if v, err := null.Value(); v != nil || err != nil {
+		t.Error("bad value or err:", v, err)
+	}
+
+	var wrong UUID
+	err = wrong.Scan(int64(42))
+	if err == nil {
+		t.Error("expected error")
+	}
+	assertNullUUID(t, wrong, "scanned wrong")
+}
+
+func assertUUID(t *testing.T, ui UUID, from string) {
+	if ui.UUID != uuidValue {
+		t.Errorf("bad %v uuid: %v ≠ %v\n", from, ui.UUID, uuidValue)
+	}
+	if !ui.Valid {
+		t.Error(from, "is invalid, but should be valid")
+	}
+}
+
+func assertNullUUID(t *testing.T, ui UUID, from string) {
+	if ui.Valid {
+		t.Error(from, "is valid, but should be invalid")
+	}
+}
